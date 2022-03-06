5923446a0119b019823c363893136693187ad8ab
gppereira <pereira7346@gmail.com>
2022-03-05 21:13:20 -0300

    feat: created main entry point

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,20 @@
-console.log("Got here xD")
+import { Formatter } from "./Formatter";
+import { Notion } from "./Notion";
+import "dotenv/config";
+
+async function main() {
+	const notion = new Notion(
+		process.env.NOTION_TOKEN as string,
+		process.env.NOTION_DATABASE_ID as string
+	);
+
+	const formatter = new Formatter(notion);
+
+	console.log("[INFO] Started formatting books");
+
+	await formatter.execute();
+
+	console.log("[INFO] Done formatting books");
+}
+
+main();
